75ad6fa7e17271b45efa033480287d4e09075a54
huy <39242676+zuofx@users.noreply.github.com>
2023-11-13 10:44:14 -0500

    Update db.js

--- backend/server/db.js
+++ backend/server/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
     host: "llm.zuofx.xyz",
     user: "huy",
-    password: "truong0714L!NUX",
+    password: "",
     database:"qgdc"
 })
 
