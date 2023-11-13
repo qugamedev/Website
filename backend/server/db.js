cad29929c0c4341b0cc27eada8fcfb430cf8ba63
Huy T <huytruong.eq@gmail.com>
2023-11-13 10:40:17 -0500

    Backend Prototype

---
+++ backend/server/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+const db = mysql.createConnection({
+    host: "llm.zuofx.xyz",
+    user: "huy",
+    password: "truong0714L!NUX",
+    database:"qgdc"
+})
+
+db.connect(function(err) {
+    if (err) throw err;
+
+    db.query("SELECT * FROM blogs", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+    }); 
+});
+
+module.exports = db;
