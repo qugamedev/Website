1949c127b982c43f160f6cd21059967b5603db19
BluBloos <cnoah1705@gmail.com>
2024-01-22 16:55:20 -0500

    add is_huy

--- website/src/components/Person.js
+++ website/src/components/Person.js
@@ -1,7 +1,7 @@
 import React from "react";
 import temp_person from "../assets/images/headshots/temp_person.webp";
 
-const Person = ({ name, title, image = temp_person }) => {
+const Person = ({ name, title, image = temp_person, is_huy=false }) => {
   return (
     <div className='lg:w-64 sm:w-48  h-auto m-4 flex flex-col items-center'>
       <div className='relative w-32 h-32 xl:w-64 sm:w-48 xl:h-64 sm:h-48'>
@@ -12,7 +12,8 @@ const Person = ({ name, title, image = temp_person }) => {
           className=' absolute w-full h-full bg-cover bg-center bg-no-repeat'
         />
       </div>
-      <div className='pt-2 font-bold text-center text-lg sm:text-xl lg:text-2xl xl:text-3xl'>{name}</div>
+      <div className='pt-2 font-bold text-center text-lg sm:text-xl lg:text-2xl xl:text-3xl'>{ 
+        is_huy ? (<div>Huy<br/>Min Truong</div>) : name}</div>
       <div className='font-normal text-center sm:text-lg lg:text-xl '>{title}</div>
     </div>
   );
